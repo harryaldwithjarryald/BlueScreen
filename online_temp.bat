a7981a7c2cece4a32cca7567b85ce76d37334bc0
Bluescreen <148587892+harryaldwithjarryald@users.noreply.github.com>
2024-03-26 16:44:21 -0400

    Update online_temp.bat

--- online_temp.bat
+++ online_temp.bat
@@ -7,7 +7,7 @@ pause
 pause
 powershell /c Invoke-WebRequest https://raw.githubusercontent.com/harryaldwithjarryald/BlueScreen/main/temp.REG -OutFile temp.REG
 echo NOTE:  If you want to backout now, close this cmd window, press "No" then run fix.reg (https://github.com/harryaldwithjarryald/BlueScreen/releases/download/standalone/fix.REG) 
-echo Then, delete the files "online_bluescreen.bat", "temp.REG", and "online_temp.bat" from the folder you downloaded this to.
+echo Then, delete the files "online_bluescreen.bat", "temp.REG", and "online_temp.bat" from the folder you ran this from.
 temp.REG
 del online_bluescreen.bat
 del temp.REG
