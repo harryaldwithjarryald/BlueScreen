af4283fde12826b9f1d03dd7c76d6fcd7edd2157
Bluescreen <148587892+harryaldwithjarryald@users.noreply.github.com>
2024-04-03 14:23:05 -0400

    Release 10.0
    Fixed a issue with 10.0, No need to update just use the online tool!

--- online_temp.bat
+++ online_temp.bat
@@ -8,7 +8,7 @@ pause
 del online_bluescreen.bat
 powershell /c wget https://raw.githubusercontent.com/harryaldwithjarryald/BlueScreen/main/temp.REG -OutFile temp.REG
 echo NOTE:  If you want to backout now, close this cmd window, press "No" on the regedit window asking to import the file then run fix.reg
-echo (https://github.com/harryaldwithjarryald/BlueScreen/releases/download/standalone/fix.REG) 
+echo (https://github.com/harryaldwithjarryald/BlueScreen/releases/download/online/fix.REG) 
 echo Then, delete the files "online_bluescreen.bat", "temp.REG", and "online_temp.bat" from the folder you ran this from.
 temp.REG
 del temp.REG
