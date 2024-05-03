ebb5c4130b3dba3f4e4faf5ccdd0dd4085104ab9
harryaldwithjarryald <148587892+harryaldwithjarryald@users.noreply.github.com>
2024-05-03 00:47:55 -0400

    Added windows installer yap

--- offline.bat
+++ offline.bat
@@ -7,6 +7,9 @@ pause
 pause
 echo NOTE:  If you want to backout now, close this cmd window, press "No" on the regedit window asking to import the file then run fix.reg in the folder you ran this from.
 echo Then, delete the files "offline.bat", and "temp.REG" from the folder you ran this from.
+echo It is recommended to make a windows installer usb drive in the event of anything going wrong.  
+echo You can download the tool to make one here: 
+echo https://www.microsoft.com/en-us/software-download/windows10
 temp.REG
 del temp.REG
 del fix.REG
