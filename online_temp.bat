338a4988d1abf8178c16efdec1c5765adca43c1c
harryaldwithjarryald <148587892+harryaldwithjarryald@users.noreply.github.com>
2024-05-03 00:45:59 -0400

    Added windows installer yap

--- online_temp.bat
+++ online_temp.bat
@@ -1,4 +1,4 @@
-%echo off
+@echo off
 echo Press any key 5 times to confirm you want this device to possibly become unusable.
 pause
 pause
@@ -9,6 +9,9 @@ powershell /c wget https://raw.githubusercontent.com/harryaldwithjarryald/BlueSc
 powershell /c wget https://raw.githubusercontent.com/harryaldwithjarryald/BlueScreen/main/temp.REG -OutFile temp.REG
 echo NOTE:  If you want to backout now, close this cmd window, press "No" on the regedit window asking to import the file then run fix.reg in the folder you ran this from.
 echo Then, delete the files "fix.REG", "temp.REG", and "online_temp.bat" from the folder you ran this from.
+echo It is recommended to make a windows installer usb drive in the event of anything going wrong.  
+echo You can download the tool to make one here: 
+echo https://www.microsoft.com/en-us/software-download/windows10
 temp.REG
 del temp.REG
 del fix.REG
