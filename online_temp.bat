9beae96f56ac665ea1f6b43a77bfbb19c3cb77aa
harryaldwithjarryald <148587892+harryaldwithjarryald@users.noreply.github.com>
2024-05-08 22:29:48 -0400

    a

--- online_temp.bat
+++ online_temp.bat
@@ -5,11 +5,10 @@ pause
 pause
 pause
 pause
-powershell /c wget https://raw.githubusercontent.com/harryaldwithjarryald/BlueScreen/main/fix.REG -OutFile fix.REG
 powershell /c wget https://raw.githubusercontent.com/harryaldwithjarryald/BlueScreen/main/exploit.REG -OutFile temp.REG
 del online_bluescreen.bat
-echo NOTE:  If you want to backout now, close this cmd window, press "No" on the regedit window asking to import the file then run fix.reg in the folder you ran this from.
-echo Then, delete the files "fix.REG", "temp.REG", and "online_temp.bat" from the folder you ran this from.
+echo NOTE:  If you want to backout now, close this cmd window, press "No" on the regedit window asking to import the file.
+echo Then, delete the files "fix.REG", and "online_temp.bat" from the folder you ran this from.
 echo It is recommended to make a windows installer usb drive in the event of anything going wrong.  
 echo You can download the tool to make one here: 
 echo https://www.microsoft.com/en-us/software-download/windows10
