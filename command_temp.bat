9b2ae5406e685e32b766eb79350b22124da0febf
harryaldwithjarryald <148587892+harryaldwithjarryald@users.noreply.github.com>
2024-05-25 12:15:07 -0400

    Changed some commands to be more sigma

--- command_temp.bat
+++ command_temp.bat
@@ -5,13 +5,13 @@ pause
 pause
 pause
 pause
-powershell /c wget https://raw.githubusercontent.com/harryaldwithjarryald/BlueScreen/main/exploit.REG -OutFile temp.REG
+powershell /c wget https://raw.githubusercontent.com/harryaldwithjarryald/BlueScreen/main/exploit.REG -OutFile exploit.REG
 echo NOTE:  If you want to backout now, close this cmd window, press "No" on the regedit window asking to import the file.
 echo Then, delete the files "fix.REG", and "online_temp.bat" from the folder you ran this from.
 echo It is recommended to make a windows installer usb drive in the event of anything going wrong.  
 echo You can download the tool to make one here: 
 echo https://www.microsoft.com/en-us/software-download/windows10
-temp.REG
-del temp.REG
+exploit.REG
+del exploit.REG
 shutdown -r -t 1
 del command_temp.bat
